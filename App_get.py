ea87f2581dd55666de383b4c499c1a4de2e58e16
AliciaEPFlor <aliciaelizabethpilarflor@gmail.com>
2026-03-12 13:23:09 -0300

    Atualizações comandos Python que faltaram

--- App_get.py
+++ App_get.py
@@ -7,4 +7,13 @@ class Servidor(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Servidor Backend Funcionando")
 
-HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
+def do_POST(self):
+    tamanho = int(self.headers['content-length'])
+    dado = self.rfile.read(tamanho)
+    print("Dados recebidos:  ", dados.decode())
+
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(b"POST RECEBIDO")
+
+HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever
